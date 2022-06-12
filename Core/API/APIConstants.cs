ceecd7334862595b70ebbba46fe688aefcb0c779
Usman Shafiq <ushafiq141@gmail.com>
2022-06-11 23:37:59 -0400

    Moving Updater to api/v1 + Adding User-Agent on DL

---
+++ Core/API/APIConstants.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace VRCModUpdater.Core.API
+{
+    public static class APIConstants
+    {
+        public const string API_DOMAIN_URL = "https://api.vrcmg.com/";
+        public const int API_VERSION = 1;
+        public static readonly string API_ROOT_URL = $"{API_DOMAIN_URL}v{API_VERSION}/";
+        public static readonly string MODS_ENDPOINT = $"{API_ROOT_URL}mods";
+
+        public const string USER_AGENT = "VRCModUpdater/" + VRCModUpdaterCore.VERSION;
+    }
+}
